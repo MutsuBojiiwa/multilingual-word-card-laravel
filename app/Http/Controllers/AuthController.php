3b1705ac9fd6762fddf1778a8428fc19eb96f1b8
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-05-26 17:13:40 +0900

    ユーザー新規登録時、メールアドレスの重複でのエラーメッセージを設定

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,6 +46,8 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
+        ], [
+            'email.unique' => 'このメールアドレスはすでに使用されています',
         ]);
 
         $user = User::create([
