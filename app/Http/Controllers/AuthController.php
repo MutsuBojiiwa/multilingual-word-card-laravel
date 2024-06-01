710ca20e4063082337f36d4517d859af3cbc299d
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-06-01 10:19:00 +0900

    不具合原因確認のため認証が必要なルートをコメントアウト

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
         $token = Auth::attempt($credentials);
 
-        Log::info('$credentials=' . $credentials);
+        Log::info('$credentials=' .json_encode($credentials));
         Log::info('$token=' . $token);
 
         if (!$token) {
--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -9,6 +9,12 @@ class HealthCheckController extends Controller
     public function health()
     {
         date_default_timezone_set('Asia/Tokyo');
-        return response()->json(['status' => 'OK','now' => date("Y-m-d H:i:s")], 200);
+        return response()->json(
+            [
+                'status' => 'OK',
+                'now' => date("Y-m-d H:i:s")
+            ],
+            200
+        );
     }
 }
